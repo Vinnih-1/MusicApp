f1670a46997f1cd3ebf2392f1810fc1a820bf813
Mikael <mikaelcandido71@gmail.com>
2023-06-16 16:59:09 -0300

    :white_check_mark:  add MusicService with the method searchAllMusics and requestPermissions

---
+++ src/services/MusicService/index.ts
@@ -0,0 +1,40 @@
+import * as MediaLibrary from 'expo-media-library';
+export enum PlayerStatus {
+    STOPPED, PAUSED, PLAYING, NONE
+}
+export interface MusicProps {
+    title: string;
+    duration: number;
+    uri: string;
+    status: PlayerStatus;
+    position: number;
+}
+
+class MusicService{
+    async requestPermissions() {
+        const { granted } = await MediaLibrary.requestPermissionsAsync();
+        if (!granted) {
+            console.error("A permissão ao armazenamento externo não foi concedida!");
+            return;
+        }
+        console.log("Permissão concedida!");
+    }
+
+    async searchAllMusics() {
+        const result = (await MediaLibrary.getAssetsAsync({mediaType: "audio", first: 1000})).assets;
+        const filteredMusic = result.filter(music => music.uri.startsWith("file:///storage/emulated/0/Music/") &&
+        music.filename.endsWith(".mp3"));
+
+        const musics = filteredMusic.map(music => ({
+            title: music.filename,
+            duration: Math.floor(music.duration),
+            uri: music.uri,
+            status: PlayerStatus.STOPPED,
+            position: 0
+        }));
+
+        return musics;
+    }
+}
+
+export default new MusicService()
